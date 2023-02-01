69d96a3d4a377e2f1fb53ffeb9d92e812e46c9ee
Nuno19kk <a32775@tomazpelayo.com>
2023-02-01 14:35:07 +0000

    F2 exercício 7

--- LetrasOla.c
+++ LetrasOla.c
@@ -1,3 +1,16 @@
 // Resolva neste ficheiro o seu exercício
+#include <stdio.h>
 
+int main()
+{
+
+	printf("     ####    #        ##\n");
+	printf("    #    #   #       #  #\n");
+	printf("    #    #   #      ######\n");
+	printf("     ####    ####  #      #\n");
+	return(0);
+
+
+
+}
 
